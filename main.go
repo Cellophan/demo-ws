c2090a6f09d399adf2904150a175b0f07dc23d6c
Cellophan <githubcom.cell@outer.systems>
2015-12-13 19:17:24 +0100

    ADD logs to server

--- main.go
+++ main.go
@@ -2,20 +2,32 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"net/http"
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request, counterCh chan int) {
 	hostname, _ := os.Hostname()
+	requestId := <-counterCh
 	fmt.Fprintf(w, "Hi from %s!", hostname)
+	log.Printf("Host: %s RemoteAddr: %s RequestId: %d", hostname, r.RemoteAddr, requestId)
 }
 
 func main() {
+	counterCh := make(chan int)
 	addrs, _ := net.InterfaceAddrs()
 
-	http.HandleFunc("/", handler)
+	go func() {
+		for x := 0; ; x++ {
+			counterCh <- x
+		}
+	}()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		handler(w, r, counterCh)
+	})
 	fmt.Printf("Server listening on  port 8080 and one of the ip from: %s\n", addrs)
 	http.ListenAndServe(":8080", nil)
 }
